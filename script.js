5232c1ce161b4be93d8534aa6d1ac0b35d3fa31b
Ariel Khawly <akhawly59@gmail.com>
2020-02-04 19:22:48 -0800

    uv api, five day api

--- script.js
+++ script.js
@@ -8,52 +8,58 @@ $(document).ready(function () {
 
   let search = $('.search-btn')
   // create an event listener that will populate the page with data from the API about the user's input
-  search.on('click', function(event)  {
+  search.on('click', function (event) {
     event.preventDefault();
     // Here we are building the URL we need to query the database
     let city = $('.user-input').val(); // here we need to make the city the user input 
     console.log(city);
     let APIKey = '8df56de68fc674d0574c7d70462e9457' // link your specific api key
     // declare a variabel containing the entire api, search query, and key
-    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    console.log(queryURL);
+    let urlBase = "http://api.openweathermap.org/data/2.5/"
+    let currentWeatherURL = urlBase + "weather?q=" + city + "&appid=" + APIKey;
+    console.log(currentWeatherURL);
+    let lon;
+    let lat;
     $.ajax({
-      url: queryURL,
+      url: currentWeatherURL,
       method: "GET"
     }).then(function (response) {
-
-      // Create CODE HERE to Log the queryURL
-      console.log(queryURL);
-      // Create CODE HERE to log the resulting object
-      console.log(response);
-      // Create CODE HERE to transfer content to HTML
       let faren = Math.floor(response.main.temp - 273.5) * 1.80 + 32
       let cityName = $('<div>' + city + '</div>');
       let tempDiv = $('<div> Temperature: ' + faren + '</div>')
       let windDiv = $('<div> Wind Speed: ' + response.wind.speed + '</div>')
-      let humDiv = $('<div> Humidity: ' + response.main.humidity + "</div>")
+      let humDiv = $('<div> Humidity: ' + response.main.humidity + "%" + "</div>")
+      lon = response.coord.lon;
+      lat = response.coord.lat;
       $('.temp').append(cityName, windDiv, humDiv, tempDiv);
-    //5 day forecast
-    let fiveDayURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-    search.on('click', function(event)  {
-      event.preventDefault();
-      // Here we are building the URL we need to query the database
-      let city = $('.user-input').val(); // here we need to make the city the user input 
-      console.log(city);
-      let APIKey = '8df56de68fc674d0574c7d70462e9457' // link your specific api key
-      // declare a variable containing the entire api, search query, and key
-      let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-      console.log(queryURL);
+      //UV INDEX
+      let uvURL = urlBase + "uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+      console.log(uvURL);
       $.ajax({
-        url: queryURL,
+        url: uvURL,
         method: "GET"
       }).then(function (response) {
-  
-        // Create CODE HERE to Log the queryURL
-        console.log(queryURL);
-        // Create CODE HERE to log the resulting object
         console.log(response);
-        // Create CODE HERE to transfer content to HTML
+        let uvDiv = $('<div> UV Index: ' + response.value + "</div>");
+        $('.temp').append(uvDiv);
+      });
+      let fiveDayURL = urlBase +"forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+      console.log(fiveDayURL);
+      $.ajax({
+        url: fiveDayURL,
+        method: "GET"
+      }).then(function (response) {
+        let noonForecast =  response.list.filter(function(day) {
+          return day.dt_txt.includes("12:00:00"); 
+        });
+        for (let day of noonForecast) {
+      
+          let fiveDiv = $('<div> Five Day Forecast: ' + day.main.humidity + "%" + "</div>");
+          $('.five-day').append(fiveDiv);
+        }
+       ;
+      });
+
     });
   });
 
