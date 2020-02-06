6b4e64076e12d1f2f5d8025bbcece5042f942a9f
Ariel Khawly <akhawly59@gmail.com>
2020-02-05 17:42:04 -0800

    icon addition, cleared multiple outputs, capitalize city

--- script.js
+++ script.js
@@ -10,54 +10,66 @@ $(document).ready(function () {
   // create an event listener that will populate the page with data from the API about the user's input
   search.on('click', function (event) {
     event.preventDefault();
+    $('.temp').html('');
+    $('.five-day').html('');
     // Here we are building the URL we need to query the database
     let city = $('.user-input').val(); // here we need to make the city the user input 
     console.log(city);
     let APIKey = '8df56de68fc674d0574c7d70462e9457' // link your specific api key
-    // declare a variabel containing the entire api, search query, and key
+    // declare a variable containing the entire api, search query, and key
     let urlBase = "http://api.openweathermap.org/data/2.5/"
     let currentWeatherURL = urlBase + "weather?q=" + city + "&appid=" + APIKey;
     console.log(currentWeatherURL);
+    // declare var for latitiude and longitude, bc they will be needed for other api calls
     let lon;
     let lat;
+    //use ajax to call your object
     $.ajax({
       url: currentWeatherURL,
       method: "GET"
     }).then(function (response) {
+      // declare a var that converts kelvin to farenheight 
       let faren = Math.floor(response.main.temp - 273.5) * 1.80 + 32
-      let cityName = $('<div>' + city + '</div>');
-      let tempDiv = $('<div> Temperature: ' + faren + '</div>')
-      let windDiv = $('<div> Wind Speed: ' + response.wind.speed + '</div>')
+      //Create divs for city name, temp, wind, and humidity
+      let cityName = $('<div>' + city.charAt(0).toUpperCase() + city.slice(1) + '</div>');
+      let tempDiv = $('<div> Temperature: ' + faren + '</div>') // replace response with your farenheight var
+      let windDiv = $('<div> Wind Speed: ' + response.wind.speed + 'MPH </div>')
       let humDiv = $('<div> Humidity: ' + response.main.humidity + "%" + "</div>")
+      // reassign values to these var, which will be plugged into the following api calls
       lon = response.coord.lon;
       lat = response.coord.lat;
+      // append your new divs to the html
       $('.temp').append(cityName, windDiv, humDiv, tempDiv);
       //UV INDEX
-      let uvURL = urlBase + "uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
-      console.log(uvURL);
-      $.ajax({
+      // the lat and long vars will still work because you are inside the same event listener
+      let uvURL = urlBase + "uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon; // here is where you will plug in your vars. 
+      $.ajax({ // call your new api with the new declared var 
         url: uvURL,
         method: "GET"
       }).then(function (response) {
-        console.log(response);
-        let uvDiv = $('<div> UV Index: ' + response.value + "</div>");
-        $('.temp').append(uvDiv);
+        let uvDiv = $('<div> UV Index: ' + response.value + "</div>"); // create a new div for UV 
+        $('.temp').append(uvDiv); // append it to the same temp div as the others
       });
-      let fiveDayURL = urlBase +"forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+      //Five Day Forecast
+      let fiveDayURL = urlBase + "forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
       console.log(fiveDayURL);
       $.ajax({
         url: fiveDayURL,
         method: "GET"
       }).then(function (response) {
-        let noonForecast =  response.list.filter(function(day) {
-          return day.dt_txt.includes("12:00:00"); 
+        let noonForecast = response.list.filter(function (day) { // filter out the response and give the object a name
+          return day.dt_txt.includes("12:00:00"); // have the function return from the 'day' object, the selected array, and have it include a selected string
         });
-        for (let day of noonForecast) {
-      
-          let fiveDiv = $('<div> Five Day Forecast: ' + day.main.humidity + "%" + "</div>");
-          $('.five-day').append(fiveDiv);
-        }
-       ;
+        for (let day of noonForecast) { // create a for loop to pull each of the below from noonforecast w/ the designated inclusion
+          let fiveFaren = Math.floor(day.main.temp - 273.5) * 1.80 + 32
+          let fiveTemp = $('<div> Temp:' + fiveFaren + "</div>")
+          let fiveHumidity = $('<div>Humidity:' + day.main.humidity + '% </div>');
+          let iconImage = $('<img src=\"http://openweathermap.org/img/wn/' + day.weather[0].icon +'@2x.png\"/>')
+          console.log(day);
+          $('.five-day').append(iconImage, fiveTemp, fiveHumidity);
+
+        };
+
       });
 
     });
